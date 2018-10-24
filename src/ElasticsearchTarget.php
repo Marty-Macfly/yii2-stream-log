4b432e7edc9680a73ba84c83e3c0816f876e12b6
Charles DELFLY <c.delfly@linkbynet.com>
2018-10-24 14:52:25 +0700

    Fix #3

--- src/ElasticsearchTarget.php
+++ src/ElasticsearchTarget.php
@@ -7,12 +7,36 @@ use Yii;
 class ElasticsearchTarget extends \yii\elasticsearch\ElasticsearchTarget
 {
     public $includeContext = true;
-    public $cacheContext   = true;
+    public $cacheContext = true;
+    /** Format of the date to add after index name, if null no date will be added */
+    public $indexDateFormat = 'y-MM-dd';
+
+    private $_index;
+
+    public function init()
+    {
+        parent::init();
+        $this->_index = $this->index;
+    }
 
     public function setContextMessage($context)
     {
         $this->cacheContext    = true;
         $this->_contextMessage = $context;
+    }
+
+    protected function setIndex()
+    {
+        $this->index=sprintf("%s%s", $this->_index, $this->indexDateFormat === null ? '' : '-' . Yii::$app->formatter->asDate(time(), $this->indexDateFormat));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function export()
+    {
+        $this->setIndex();
+        return parent::export();
     }
 
     /**
